6e92989576cce20ebef2cbf01ab72c705895c968
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-17 13:50:31 +0000

    Criando controller de funcionário

--- src/controllers/employee.controllers.js
+++ src/controllers/employee.controllers.js
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import { Request, Response } from 'express';
+import db from '../database/connections';
 
 export default class EmployeeController {
   async index(req = Request, res = Response) {
@@ -7,6 +8,22 @@ export default class EmployeeController {
   }
 
   async create(req = Request, res = Response) {
-    res.send('ok');
+    const data = req.body;
+
+    const trx = await db.transaction(); // Faz com que se ocorrer um erro em qualquer tabela, os dados não serão salvos
+
+    try {
+      await trx('employee').insert(data);
+      await trx.commit(); //Salvando os dados no banco
+
+      res.status(201).send();
+    } catch (err) {
+      await trx.rollback(); //Desfazendo qualquer erro
+      console.log(`Erro no knowledge_areas_controller ${err}`);
+      res.status(500).json({
+        error: err,
+        message: 'Erro não esperado.',
+      });
+    }
   }
 }
