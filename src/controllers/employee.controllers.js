fc291e6209675a872e0015c112c1cc2e3416a42f
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-17 13:16:14 +0000

    Criando controller de funcionário

---
+++ src/controllers/employee.controllers.js
@@ -0,0 +1,12 @@
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+
+export default class EmployeeController {
+  async index(req = Request, res = Response) {
+    res.send('employee controller');
+  }
+
+  async create(req = Request, res = Response) {
+    res.send('ok');
+  }
+}
