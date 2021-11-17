824385724da30be6207b2bf3e2408d4e3f5f9d44
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-17 13:20:12 +0000

    Criando controller de funcionário

--- src/controllers/employee.controllers.js
+++ src/controllers/employee.controllers.js
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 export default class EmployeeController {
   async index(req = Request, res = Response) {
-    res.send('employee controller');
+    res.send('employee controller.');
   }
 
   async create(req = Request, res = Response) {
