b716d22d3660e9140a4f9144732621be54e94e0d
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-17 13:55:04 +0000

    Adicionando as configurações de rota do Funcionário

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import KnowledgeAreasController from '../controllers/knowledgeAreas.controllers';
+import EmployeeController from '../controllers/employee.controllers';
 
 const routes = Router();
+
 const knowledgeAreasController = new KnowledgeAreasController();
+const employeeController = new EmployeeController();
 
 routes.get('/knowledge_areas', knowledgeAreasController.index);
+
+/* Employee Controller */
+routes.get('employees', employeeController.index);
+routes.post('employees', employeeController.create);
 
 export default routes;
