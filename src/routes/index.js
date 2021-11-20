b6c1a8c1a95a010d979d840f840f61bd6f85a70c
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-20 12:27:31 +0000

    Configurando rotas do student

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,24 @@
 import { Router } from 'express';
 import KnowledgeAreasController from '../controllers/knowledgeAreas.controllers';
 import EmployeeController from '../controllers/employee.controllers';
+import StudentController from '../controllers/student.controller';
 
 const routes = Router();
 
 const knowledgeAreasController = new KnowledgeAreasController();
 const employeeController = new EmployeeController();
+const studentController = new StudentController();
 
+/* ROUTES */
 routes.get('/knowledge_areas', knowledgeAreasController.index);
 routes.post('/knowledge_areas/:id', knowledgeAreasController.create);
 
 /* Employee Controller */
 routes.get('/employees', employeeController.index);
 routes.post('/employees', employeeController.create);
+
+/* Student Controller */
+routes.get('/students', studentController.index);
+routes.post('/students', studentController.create);
 
 export default routes;
