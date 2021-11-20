58cc3fc936ac00e5bc45928de7453178d7d97953
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-20 14:20:39 +0000

    Criando controller de lends

--- src/controllers/lends.controllers.js
+++ src/controllers/lends.controllers.js
@@ -4,12 +4,9 @@ import db from '../database/connections';
 export default class LendsController {
   async index(req = Request, res = Response) {
     try {
-      const data = await db
-        .select()
-        .from()
-        .table('lends')
-        .join('publications', 'lends.publications_id', '=', 'publications.id')
-        .select(['lends.*', 'publications.*']);
+      const data = await db.select().from().table('lends');
+      /* .join('publications', 'lends.publications_id', '=', 'publications.id')
+        .select(['lends.*', 'publications.*']); */
 
       res.status(200).json(data);
     } catch (err) {
@@ -22,13 +19,12 @@ export default class LendsController {
   }
 
   async create(req = Request, res = Response) {
-    const { inicio, termino, publicationsId } = req.body;
+    const { termino, publicationsId } = req.body;
     const employeeId = req.params.id;
 
     const trx = await db.transaction();
     try {
       await trx('lends').insert({
-        inicio,
         termino,
         employee_id: employeeId,
         publication_id: publicationsId,
