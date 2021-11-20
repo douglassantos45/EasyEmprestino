76a2b3df192fdf42aba06a211f03b54450b64eb1
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-20 13:18:47 +0000

    Configurando rotas publications controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import KnowledgeAreasController from '../controllers/knowledgeAreas.controllers';
 import EmployeeController from '../controllers/employee.controllers';
 import StudentController from '../controllers/student.controller';
+import PublicationController from '../controllers/publications.controllers';
 
 const routes = Router();
 
 const knowledgeAreasController = new KnowledgeAreasController();
 const employeeController = new EmployeeController();
 const studentController = new StudentController();
+const publicationController = new PublicationController();
 
 /* ROUTES */
 routes.get('/knowledge_areas', knowledgeAreasController.index);
@@ -20,5 +22,9 @@ routes.post('/employees', employeeController.create);
 /* Student Controller */
 routes.get('/students', studentController.index);
 routes.post('/students', studentController.create);
+
+/* Publication Controller */
+routes.get('/students', publicationController.index);
+routes.post('/students/:id', publicationController.create);
 
 export default routes;
