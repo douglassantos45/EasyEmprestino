cdd049620fac9aa88d093cbec69348063b3e03ac
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-20 13:56:20 +0000

    Configurando rotas lends controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import KnowledgeAreasController from '../controllers/knowledgeAreas.controllers'
 import EmployeeController from '../controllers/employee.controllers';
 import StudentController from '../controllers/student.controller';
 import PublicationController from '../controllers/publications.controllers';
+import LendsController from '../controllers/lends.controllers';
 
 const routes = Router();
 
@@ -10,6 +11,7 @@ const knowledgeAreasController = new KnowledgeAreasController();
 const employeeController = new EmployeeController();
 const studentController = new StudentController();
 const publicationController = new PublicationController();
+const leadsController = new LendsController();
 
 /* ROUTES */
 routes.get('/knowledge_areas', knowledgeAreasController.index);
@@ -26,5 +28,9 @@ routes.post('/students', studentController.create);
 /* Publication Controller */
 routes.get('/publications', publicationController.index);
 routes.post('/publications/:id', publicationController.create);
+
+/* Lends Controller */
+routes.get('/lends', leadsController.index);
+routes.post('/lends/:id', leadsController.create);
 
 export default routes;
