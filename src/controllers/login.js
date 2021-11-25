8682db5462c9c9163093e8d62af5eeba402cd1f7
Douglas Tott <douglasifbaianosb@gmail.com>
2021-11-25 13:20:58 +0000

    Adicionando login para employee

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,12 +3,18 @@
 import { Request, Response } from 'express';
 import db from '../database/connections';
 
-export default class Login {
+export default class LoginController {
   async post(req = Request, res = Response) {
     try {
       const { email, senha } = req.body;
-      const data = await db('employee').where('email', '=', email);
+      const [data] = await db('employee').where('email', '=', email);
+      if (!data || data.senha != senha) {
+        return res.status(401).json({
+          message: 'Usuário ou senha inválidos',
+        });
+      }
 
+      console.log(email, senha);
       res.json(data);
     } catch (err) {
       console.log(`Erro no login ${err}`);
